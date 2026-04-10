54582f520f77a529bcaecaeb299294643f70268f
Francis <lavinajohnfrancis0605@gmail.com>
2026-04-10 11:11:03 +0800

    Initial commit

---
+++ Verilog2.v
@@ -0,0 +1,32 @@
+// testbench for xor  module 
+
+module testbench ();
+	//tb signals
+	
+	reg b;
+	reg a;
+	wire y;
+
+	// instantiate xor module 
+	xor_gate dut(
+	
+		.Y(y),
+		.A(a),
+		.B(b)
+	);
+
+	// apply stimuli 
+	initial begin 		
+		a=0; b=0; #10;
+		a=0; b=1; #10;
+		a=1; b=0; #10;
+		a=1; b=1; #10;
+
+
+	
+
+
+	end 
+
+
+endmodule 
